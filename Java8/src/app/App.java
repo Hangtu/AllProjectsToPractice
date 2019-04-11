e19fad06878cfcdd8b63388be38d1dc8c705048a
Hangtu <hangtuwlf@gmail.com>
2019-04-11 12:30:17 -0500

    Stream Map

--- Java8/src/app/App.java
+++ Java8/src/app/App.java
@@ -2,28 +2,28 @@ package app;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class App implements MyInterface {
     public static void main(String[] args) throws Exception {
- 
+
         System.out.println("Hello Java 8");
 
-
-        int [] b = new int [2];
+        int[] b = new int[2];
         String myName = "Hang Tu";
         int i = 0;
         int j = 1;
 
         try {
-            int r = j/i;
+            int r = j / i;
         } catch (ArithmeticException e) {
-           // System.err.println(e.toString());
-        }catch(Exception eArithmeticException){
+            // System.err.println(e.toString());
+        } catch (Exception eArithmeticException) {
             System.out.println("Hello");
             System.err.println("Erro");
-        }
-            finally{
-           // System.out.println("Pasa");
+        } finally {
+            // System.out.println("Pasa");
         }
 
         // app.helloWorld(); // default method
@@ -31,38 +31,45 @@ public class App implements MyInterface {
         Student me = SingletonExample.getFirstStudent();
         // System.out.println(me.getName()); // get static member of the class;
 
-
-        MyInterface myInterface = (x) -> { System.out.println(me.getId() * 4); };
+        MyInterface myInterface = (x) -> {
+          //  System.out.println(me.getId() * 4);
+        };
         myInterface.multiplication(me); // Print the lambda expression;
 
+        /* Collections */
 
-        /*Collections*/
-
-        ArrayList <Student> aList = new ArrayList <Student> (); 
+        ArrayList<Student> aList = new ArrayList<Student>();
         aList.add(me);
-        aList.add(new Student(2,"Juan"));
-        aList.add(new Student(3,"Pepe"));
-        aList.add(new Student(4,"Roe"));
-        aList.add(new Student(5,"Sandra"));
+        aList.add(new Student(2, "Juan"));
+        aList.add(new Student(3, "Pepe"));
+        aList.add(new Student(4, "Roe"));
+        aList.add(new Student(5, "Sandra"));
 
-        //System.out.println(aList.get(1).getName());
+        // System.out.println(aList.get(1).getName());
 
         aList.forEach(x -> {
-            //x.setName(Integer.toString(x.getId()));
+            // x.setName(Integer.toString(x.getId()));
         });
 
         aList.forEach(x -> {
-             // System.out.println(x.getName());
+            // System.out.println(x.getName());
         });
 
-        HashMap <Integer, Student> hashMap = new HashMap<Integer, Student>();
+        HashMap<Integer, Student> hashMap = new HashMap<Integer, Student>();
         hashMap.put(1, me);
         hashMap.put(null, aList.get(1));
 
         hashMap.forEach((x, student) -> {
-            System.out.println(student.getName());
+          //  System.out.println(student.getName());
         });
 
+         //MAP CREATE A NEW LIST
+        List<Student> bList = aList.stream().map(x -> new Student(x.getId(),x.getName()+" B")).collect(Collectors.toList());
+        bList.forEach(student -> {
+            System.out.println(student.getName());
+        });
+        
+        
     }
 
     @Override
