c279dbdda0048819f8c86c9f6b334a9f539744ea
Hangtu <hangtuwlf@gmail.com>
2019-04-24 10:02:48 -0500

    await keyword

--- Angular/AngularTemplate/src/app/app.component.ts
+++ Angular/AngularTemplate/src/app/app.component.ts
@@ -13,5 +13,23 @@ export class AppComponent {
   constructor(){
     console.log(...this.data); //SPREAD OPERATOR 
     /*Spread syntax allows an iterable such as an array expression or string to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected, or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected.*/
+    this.msg(); // Message: 🤡 <-- after 2 seconds
   }
+
+
+
+   scaryClown() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('🤡');
+      }, 2000);
+    });
+  }
+  
+  async msg() {
+    const msg = await this.scaryClown(); // await is a new operator used to wait for a promise to resolve or reject.
+    console.log('Message:', msg);// THIS IS GOING TO WAIT UNTIL THE SCARY CLOWN METHOD IS COMPLETED.
+  }
+  
+ 
 }
