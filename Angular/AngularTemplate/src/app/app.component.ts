abfae880b9db3cdb6c534d4f20daa7d8a0b4b4c7
Hangtu <hangtuwlf@gmail.com>
2019-04-24 09:54:43 -0500

    Spread Operator javascript

--- Angular/AngularTemplate/src/app/app.component.ts
+++ Angular/AngularTemplate/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularTemplate';
+
+  private data = ['1','2','3','4'];
+
+  constructor(){
+    console.log(...this.data); //SPREAD OPERATOR 
+    /*Spread syntax allows an iterable such as an array expression or string to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected, or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected.*/
+  }
 }
